c8f2b1162413668330093d46af0e9e48ae0e8d39
John Dzialo <jmd9295@gmail.com>
2018-03-12 09:38:27 -0400

    fix linting issues with use strict and add a @returns to jsdoc

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+
+'use strict';
 /**
  * HTTP Cloud Function.
  *
  * @param {Object} req Cloud Function request context.
  * @param {Object} res Cloud Function response context.
+ * @returns {String} response context sent string
  */
 exports.helloGET = (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 };
