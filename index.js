0a94a7937232ea9f72e03512a241419b7b61fb25
John Dzialo <jmd9295@gmail.com>
2018-03-13 15:38:34 -0400

    update serverless.yml

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-
 'use strict';
-/**
- * HTTP Cloud Function.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- * @returns {String} response context sent string
- */
-exports.helloGET = (req, res) => {
-  res.send('Hello World!');
+
+exports.http = (request, response) => {
+  response.status(200).send('Hello World!');
+};
+
+exports.event = (event, callback) => {
+  callback();
 };
