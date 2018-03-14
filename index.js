5289498ab6c0548b8dbfba5b5debba0e340a5d55
Matt Hare <mhare@oreilly.com>
2018-03-14 10:53:46 -0400

    this might make it more clear

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
-exports.http = (request, response) => {
+
+// this example shows an http response event type
+exports.helloWorld = (request, response) => {
   response.status(200).send('Hello World!');
 };
 
-exports.event = (event, callback) => {
+// this example shows an pub/sub event type
+exports.pubSub = (event, callback) => {
   callback();
 };
